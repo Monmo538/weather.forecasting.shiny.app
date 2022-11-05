68a475152898b217c1d42010e46ce6e249ab5738
Ad <destontrace@gmail.com>
2022-11-05 16:56:27 +0100

    added some code

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library("shiny")
+library("ggplot2")
 
-cities <- c("Stockholm", "Gothenburg", "Karlskrona", "Malmö", "Linköping", "Hamburg", "Berlin", "Cologne", "Leipzig", "Paris", "Copenhagen", "Zurich", "Helsinki", "Oslo")
+cities <- c("Stockholm", "Gothenburg", "Karlskrona", "Malmo", "Linkoping", "Hamburg", "Berlin", "Cologne", "Leipzig", "Paris", "Copenhagen", "Zurich", "Helsinki", "Oslo")
 coord <- c("59.3294,18.0686", "57.6717,11.9810", "56.1611,15.5881", "55.5932,13.0214", "58.4094,15.6257", "53.5500,10.0000", "52.5167,13.3833", "50.9422,6.9578", "51.3333,12.3833", "48.8566,2.3522", "55.6761,12.5683", "47.3769,8.5417", "60.1699,24.9384", "59.9139,10.7522")
 country <- c("Sweden","Sweden","Sweden","Sweden","Sweden", "Germany", "Germany", "Germany", "Germany", "France", "Denmark", "Switzerland", "Finland", "Norway")
 units <- c("metric", "imperial")
@@ -18,27 +19,27 @@ ui <-  fluidPage(
 )
 
 server <-  function(input, output) {
-  
+
   div_ru <- reactive({
     input$city
   })
   div_ru2 <- reactive({
     input$api_Key
   })
-  
+
   output$plot <- plotly::renderPlotly({
     df <- weather_forcast(cityData[cityData$cities == input$city,]$coord, input$unit, input$api_Key)
-    
-    yLab = "Temprature in Fahrenheit"    
+
+    yLab = "Temprature in Fahrenheit"
     if (input$unit == "metric") {
-      yLab = "Temprature in Celsius"    
+      yLab = "Temprature in Celsius"
     }
-    
+
     df$timestamp <- as.Date(strptime(df$startTime, "%Y-%m-%dT%H:%M:%S"))
     df$timestamp <- unlist(df$timestamp)
     temp <- df$values
     df$temp <- temp$temperature
-    
+
     plotly::ggplotly(
       ggplot2::ggplot(data=df, ggplot2::aes(timestamp, temp)) +
         ggplot2::geom_line()+
@@ -51,5 +52,3 @@ server <-  function(input, output) {
 
 
 shinyApp(ui, server)
-
-
